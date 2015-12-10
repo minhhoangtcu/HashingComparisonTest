ccdec42a22b321a9f8d3c20916a3c51a3ff0d1f4
minhhoangtcu <minh.hoang@tcu.edu>
2015-12-10 01:14:28 -0600

    Initiated Quotient Probing (change work space)

---
+++ src/hashing/hashTables/QuotientProbingHashTable.java
@@ -0,0 +1,17 @@
+package hashing.hashTables;
+
+public class QuotientProbingHashTable extends HashTable {
+	
+	public QuotientProbingHashTable(int sizeOfTable) {
+		super(sizeOfTable);
+	}
+
+	public int put(String key) throws TableFullException {
+		return 0;
+	}
+
+	public int search(String key) {
+		return 0;
+	}
+
+}
