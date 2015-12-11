b3258ca9862bfc9f1dce6d960418cc92c0f15018
minhhoangtcu <minh.hoang@tcu.edu>
2015-12-11 13:01:54 -0600

    Changed the content of individual page (no missing/present keys)

--- src/hashing/gui/views/IndividualPage.java
+++ src/hashing/gui/views/IndividualPage.java
@@ -5,27 +5,24 @@ import java.io.IOException;
 import hashing.hashTables.PackingDensity;
 import hashing.reader.TemplateReader;
 import hashing.reader.TemplateReader.Template;
+import hashing.resolutionMethods.HashingResolutionMethod;
 
 public class IndividualPage {
 	
 	public static final String PATH = "templates/IndividualAlgorithm.html";
 
-	public static String fillAll(String name, PackingDensity density,
+	public static String fillAll(HashingResolutionMethod method, PackingDensity density,
 			int collisions, int keys,
-			int comparesMissing, int searchsMissing,
-			int comparesPresent, int searchsPresent) throws IOException {
+			int compares, int searches) throws IOException {
 		String input = TemplateReader.read(Template.INDIVIDUAL_PAGE);
-		input = input.replace("$name", name);
+		input = input.replace("$name", method.getName());
 		input = input.replace("$density", density.getName());
 		input = input.replace("$totalCollisions", collisions+"");
 		input = input.replace("$totalKeys", keys+"");
 		input = input.replace("$averageCollisions", (double) collisions/keys +"");
-		input = input.replace("$totalComparesMissing", comparesMissing+"");
-		input = input.replace("$totalSearchsMissing", searchsMissing+"");
-		input = input.replace("$averageComparesMissing", (double) comparesMissing/searchsMissing +"");
-		input = input.replace("$totalComparesPresent", comparesPresent+"");
-		input = input.replace("$totalSearchsPresent", searchsPresent+"");
-		input = input.replace("$averageComparesPresent", (double) comparesPresent/searchsPresent +"");
+		input = input.replace("$totalCompares", compares+"");
+		input = input.replace("$totalSearchs", searches+"");
+		input = input.replace("$averageCompares", (double) compares/searches +"");
 		return input;
 		
 	}
