9d8e4109c49ebe7f866da421a6f59d4fc1c850d6
minhhoangtcu <minh.hoang@tcu.edu>
2015-12-11 13:14:35 -0600

    enabled to create a hash table based on the method.

--- src/hashing/hashTables/HashTable.java
+++ src/hashing/hashTables/HashTable.java
@@ -4,12 +4,12 @@ import hashing.resolutionMethods.Hash;
 import hashing.resolutionMethods.HashingResolutionMethod;
 
 public abstract class HashTable {
-	
+
 	protected int sizeOfTable;
 	protected int numberOfPairs;
 	protected String[] values;
 	protected HashingResolutionMethod method;
-	
+
 	public HashTable(int sizeOfTable, HashingResolutionMethod method) {
 		this.sizeOfTable = sizeOfTable;
 		this.method = method;
@@ -20,10 +20,25 @@ public abstract class HashTable {
 	protected int hash(String key) {
 		return Hash.divisionHash(key);
 	}
-	
+
 	public abstract int put(String key) throws TableFullException;
+
 	public abstract int search(String key);
+
 	public HashingResolutionMethod getMethod() {
 		return method;
+	}
+
+	public static HashTable getHashTable(HashingResolutionMethod method, int sizeOfTable) {
+		switch (method) {
+		case LINEAR_PROBING:
+			return new LinearProbingHashTable(sizeOfTable);
+		case QUADRATIC_PROBING:
+			return new QuadraticProbingHashTable(sizeOfTable);
+		case LINEAR_QUOTIENT:
+			return new LinearQuotientHashTable(sizeOfTable);
+		default:
+			return null;
+		}
 	}
 }
