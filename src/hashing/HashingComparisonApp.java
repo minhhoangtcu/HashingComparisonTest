a5baf31fd933aefcdb827f98f6e6f1f54555ce31
minhhoangtcu <minh.hoang@tcu.edu>
2015-12-11 17:04:30 -0600

    Wrote Document

--- src/hashing/HashingComparisonApp.java
+++ src/hashing/HashingComparisonApp.java
@@ -1,3 +1,11 @@
+/**
+ * Author: Minh Hoang
+ * Project Name: Hashing Collision Handling Comparison
+ * Features:
+ * 	Select a file containing a lot of keys to test inserting into the predefined table.
+ * 	Display the data nicely using html. Consumer can change the template of what to display with the html files.
+ * 	Analyze algorithms based on time and packing density. To enable this, please remove the comment in class Model.
+ */
 package hashing;
 
 import java.io.IOException;
@@ -9,16 +17,16 @@ import hashing.gui.views.WelcomePage;
 import hashing.hashTables.TableFullException;
 
 public class HashingComparisonApp {
-	
+
 	Control control;
 	Model model;
 	View view;
-	
+
 	public static void main(String[] args) throws IOException, TableFullException {
 		HashingComparisonApp app = new HashingComparisonApp();
 		app.view.setVisible(true);
 	}
-	
+
 	public HashingComparisonApp() throws IOException, TableFullException {
 		model = new Model();
 		view = new View();
