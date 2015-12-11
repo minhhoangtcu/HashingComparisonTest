7011a31d116f04b13a7dc24465e2d878798ab8fe
minhhoangtcu <minh.hoang@tcu.edu>
2015-12-11 14:21:49 -0600

    Count time it takes to insert/search

--- src/hashing/gui/views/WelcomePage.java
+++ src/hashing/gui/views/WelcomePage.java
@@ -17,12 +17,12 @@ import hashing.resolutionMethods.HashingResolutionMethod;
 public class WelcomePage {
 	
 	public static final String PATH = "templates/WelcomeScreen.html";
-	public static final String[] LINEAR_PROBE_50_IDS = {"$lp50Collisions", "$lp50ComparisonsMissing", "$lp50ComparisonsPresent"};
-	public static final String[] LINEAR_PROBE_90_IDS = {"$lp90Collisions", "$lp90ComparisonsMissing", "$lp90ComparisonsPresent"};
-	public static final String[] QUADRATIC_PROBE_50_IDS = {"$qp50Collisions", "$qp50ComparisonsMissing", "$qp50ComparisonsPresent"};
-	public static final String[] QUADRATIC_PROBE_90_IDS = {"$qp90Collisions", "$qp90ComparisonsMissing", "$qp90ComparisonsPresent"};
-	public static final String[] LINEAR_QUOTIENT_50_IDS = {"$lq50Collisions", "$lq50ComparisonsMissing", "$lq50ComparisonsPresent"};
-	public static final String[] LINEAR_QUOTIENT_90_IDS = {"$lq90Collisions", "$lq90ComparisonsMissing", "$lq90ComparisonsPresent"};
+	public static final String[] LINEAR_PROBE_50_IDS = {"$lp50Collisions", "$lp50ComparisonsMissing", "$lp50ComparisonsPresent", "$lp50TimeInsert", "$lp50TimeSearchMissing", "$lp50TimeSearchPresent"};
+	public static final String[] LINEAR_PROBE_90_IDS = {"$lp90Collisions", "$lp90ComparisonsMissing", "$lp90ComparisonsPresent", "$lp90TimeInsert", "$lp90TimeSearchMissing", "$lp90TimeSearchPresent"};
+	public static final String[] QUADRATIC_PROBE_50_IDS = {"$qp50Collisions", "$qp50ComparisonsMissing", "$qp50ComparisonsPresent", "$qp50TimeInsert", "$qp50TimeSearchMissing", "$qp50TimeSearchPresent"};
+	public static final String[] QUADRATIC_PROBE_90_IDS = {"$qp90Collisions", "$qp90ComparisonsMissing", "$qp90ComparisonsPresent", "$qp90TimeInsert", "$qp90TimeSearchMissing", "$qp90TimeSearchPresent"};
+	public static final String[] LINEAR_QUOTIENT_50_IDS = {"$lq50Collisions", "$lq50ComparisonsMissing", "$lq50ComparisonsPresent", "$lq50TimeInsert", "$lq50TimeSearchMissing", "$lq50TimeSearchPresent"};
+	public static final String[] LINEAR_QUOTIENT_90_IDS = {"$lq90Collisions", "$lq90ComparisonsMissing", "$lq90ComparisonsPresent", "$lq90TimeInsert", "$lq90TimeSearchMissing", "$lq90TimeSearchPresent"};
 	
 	public static String fillAll(Model model) throws IOException, TableFullException {
 		String input = TemplateReader.read(Template.WELCOME_PAGE);
@@ -38,32 +38,45 @@ public class WelcomePage {
 	}
 	
 	public static String fillUpOneMethod(Model model, String input, HashTable table, PackingDensity density) throws IOException, TableFullException {
-		String[] randomKeys = model.getInsertingKeys();
+		int POPULATE = 100;
+		
+		String[] randomKeys = populateArray(model.getInsertingKeys(), POPULATE);
 		int totalCollisions = 0;
+		long tStart = System.currentTimeMillis();
 		for (String key : randomKeys) {
 			totalCollisions += table.put(key);
 		}
+		long insertingTime = (long) System.currentTimeMillis() - tStart;
 
-		String[] presentKeys = AlphanumericReader.getKeys(AlphanumericReader.PRESENT_KEYS);
+		
+		String[] presentKeys = populateArray(AlphanumericReader.getKeys(AlphanumericReader.PRESENT_KEYS), POPULATE);
+		tStart = System.currentTimeMillis();
 		int totalComparesPresent = 0;
 		for (String key : presentKeys) {
 			totalComparesPresent += table.search(key);
 		}
+		long searchingMissTime = (long) System.currentTimeMillis() - tStart;
 
-		String[] missingKeys = AlphanumericReader.getKeys(AlphanumericReader.MISSING_KEYS);
+		String[] missingKeys = populateArray(AlphanumericReader.getKeys(AlphanumericReader.MISSING_KEYS), POPULATE);
+		tStart = System.currentTimeMillis();
 		int totalComparesMissing = 0;
 		for (String key : missingKeys) {
 			totalComparesMissing += table.search(key);
 		}
+		long searchingPresentTime = (long) System.currentTimeMillis() - tStart;
 
 		return WelcomePage.fillUpOneRow(input, table.getMethod(), density,
 				(double) totalCollisions / randomKeys.length,
 				(double) totalComparesMissing / missingKeys.length,
-				(double) totalComparesPresent / presentKeys.length);
+				(double) totalComparesPresent / presentKeys.length,
+				insertingTime, searchingMissTime, searchingPresentTime);
 	}
 	
-	public static String fillUpOneRow(String input, HashingResolutionMethod method, PackingDensity density, double collisions, double comparesMissing, double comparesPresent) throws IOException {
-		String[] ids = new String[3];
+	public static String fillUpOneRow(String input,
+			HashingResolutionMethod method, PackingDensity density,
+			double collisions, double comparesMissing, double comparesPresent,
+			long insertingTime, long searchingMissTime, long searchingPresentTime) throws IOException {
+		String[] ids = new String[6];
 		
 		switch (method) {
 		case LINEAR_PROBING:
@@ -91,7 +104,27 @@ public class WelcomePage {
 		input = input.replace(ids[0], collisions+"");
 		input = input.replace(ids[1], comparesMissing+"");
 		input = input.replace(ids[2], comparesPresent+"");
+		input = input.replace(ids[3], insertingTime+"");
+		input = input.replace(ids[4], searchingMissTime+"");
+		input = input.replace(ids[5], searchingPresentTime+"");
 		return input;
+	}
+	
+	/**
+	 * Expand the array with the number of times provided. This method will copy elements from old array to make a new array with a lot more elements.
+	 * @param times
+	 * @return a new and bigger array
+	 */
+	public static String[] populateArray(String[] old, int times) {
+		String[] newArray = new String[old.length*times];
+		int oldIndex = 0;
+		for (int newIndex = 0; newIndex < newArray.length; newIndex++) {
+			newArray[newIndex] = old[oldIndex];
+			oldIndex++;
+			if (oldIndex == old.length)
+				oldIndex = 0;
+		}
+		return newArray;
 	}
 	
 }
